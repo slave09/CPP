b3e6728a367b80bc98959eee8068d9464920f0ad
najm09 <fnajm09@gmail.com>
2020-02-20 18:03:46 +0530

    Basics of Cpp

--- Arrays_class.cpp
+++ Arrays_class.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-//implementation of array
+
 class Array{
 	private:
 		int *A;
@@ -17,11 +17,15 @@ class Array{
 		Array(int sz)
 		{
 			size=sz;
-			//creating array in heap
+
+			//creating dynamic array inside heap which can be accesed at any time 
+
 			A=new int[size];
 			length=0;
 		}
+
          //deallocation of array
+
 		~Array()
 		{
 			delete []A;
@@ -32,14 +36,20 @@ class Array{
 		int Delete(int index);//declaring delete function
 };
 //function for displaying Array 
+//scaning all the elements and printing row by row
+
 void Array :: Display(){
 	for(int i=0;i<length;i++){
-		cout<<A[i]<<" ";//printing a space
+		cout<<A[i]<<" ";//
 	}
 	//printing a new line
 	cout<<endl;
 }
 // function for inserting a value in the given array at a given index
+//scaning the array to that index where value is to be inserted 
+//and making the index of further elements increased by one 
+//length of the array will be increased
+
 void Array :: Insert(int index,int x){
 	if(index>=0 && index<=length){
 		for (int i = length; i >= index; i--)
@@ -49,6 +59,10 @@ void Array :: Insert(int index,int x){
 	}
 }
 //function for deleting an element of desired index
+//scaning all the elments through out and changing the index of the element
+//by 1 from that index onwards whose value is to be deleted
+//the lenght will also decrease due to shifting backwards
+
 int Array :: Delete(int index){
 	int x=0;
 	if(index>=0 && index<length){
--- matrices_diagonal.cpp
+++ matrices_diagonal.cpp
@@ -23,10 +23,20 @@ class Diagonal{
 		void Display();
 };
 
+//the matrix is diagonal only if all elements except diagonal elements are zero
+//the diagonal index is that where row and column indexes are same  
+//so we only have to assign diagonal elements where row and column indexes are same
+//one dimensional array is sufficient to store all the diagonal elements
+
 void Diagonal :: Set(int i,int j,int x){
 	if(i==j)
 		A[i-1]=x;
 }
+
+//get function will retrieve the value of given index
+//for diagonal matrix all the elements are zero except diagonal element
+//thus there is a condition when indexes are not same it will retrieve "0"
+//and if indexes are same then it will retrieve values from the one dimensional array
 
 int Diagonal :: Get(int i, int j){
 	if (i==j)
@@ -34,6 +44,8 @@ int Diagonal :: Get(int i, int j){
 	else
 		cout<<"0 ";
 }
+
+//Dispalay function is to print the entire diagonal matrix
 
 void Diagonal :: Display(){
 	for (int i = 0; i < n; i++)
