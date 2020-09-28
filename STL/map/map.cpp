6207d3c1f998266ca4eeba62f26b9aa7b8f420e7
najm09 <fnajm09@gmail.com>
2020-09-28 22:03:22 +0530

    parenthesis matching

--- STL/map/map.cpp
+++ STL/map/map.cpp
@@ -1,13 +1,14 @@
 /*
-  Author: Fakhra Najm<fnajm09@gmail.com>
+  * Author: Fakhra Najm<fnajm09@gmail.com>
   * Topic : Map in C++ Standard Template Library (STL)
+  * Functionality of ordered map
 */
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-  map<int ,int> MAP;
+  map<int ,int> MAP,copy_MAP;
   // map<int,int>::iterator i; 
   if(MAP.empty()) cout<<"Empty! Please insert some values."<<endl;
   cout<<"map can have a maximum size of "<<MAP.max_size()<<endl;
@@ -17,11 +18,14 @@ int main(){
   MAP.insert(pair<int,int>(3,30));
   MAP.insert(pair<int,int>(4,40));
   MAP.insert(pair<int,int>(5,50));
+  copy_MAP = MAP;
   cout<<"Good You have inserted "<<MAP.size()<<" elements"<<endl;
   cout<<"Erase first three values that you have inserted"<<endl;
   MAP.erase(MAP.begin(), MAP.find(3));
   cout<<"Insert a value into the map"<<endl;
   MAP.insert({2,20});
+  cout<<"Lets use emplace function for learning purposes"<<endl;
+  MAP.emplace(1,25);
   cout<<"key"<<'\t'<<"Value"<<endl;
   for(auto i = MAP.begin(); i != MAP.end(); ++i)
     cout << i->first<<'\t'<< i->second<<endl;
@@ -30,6 +34,10 @@ int main(){
   cout<<"MAP.upper_bound(4) "<<"Value: "<<MAP.upper_bound(4)->second<<endl;
   MAP.clear();
   cout<<"It seems you have cleared the map and it shows "<<MAP.size()<<" size"<<endl;
-  
+  cout<<"original inserted map"<<endl;
+   cout<<"key"<<'\t'<<"Value"<<endl;
+  for(auto i = copy_MAP.begin(); i != copy_MAP.end(); ++i)
+    cout << i->first<<'\t'<< i->second<<endl;
+
   return 0;
 }
