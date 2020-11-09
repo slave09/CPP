6c55a8e8551ac844811c9534b49e6fdcf054dad0
najm09 <fnajm09@gmail.com>
2020-11-09 21:47:35 +0530

    X of a Kind in a Deck of Cards

---
+++ leetcode/Array/Easy/set.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& deck) {
+      map<int,int>m;
+      int answere = 0;
+      for(auto i: deck) m[i]++;
+      for(auto i : m) answere = __gcd(i.second, answere);
+      return answere > 1;
+    }
+};
