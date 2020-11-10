04ed9043691c14b200457e8e1c74787c16840d3e
najm09 <fnajm09@gmail.com>
2020-11-10 20:46:58 +0530

    easy problems

--- leetcode/Easy/difference.cpp
+++ leetcode/Easy/difference.cpp
@@ -11,4 +11,10 @@ public:
         }
         return t[t.size()-1];
     }
+    char OtherSolution(string s, string t) {
+        char c = t[t.size() - 1];
+        for(int i = 0; i < s.size(); ++i)
+          c ^= s[i]^t[i];
+        return c;
+    }
 };
