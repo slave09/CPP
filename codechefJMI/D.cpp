12eeb72f54419f833163b105ed51bf86183c799f
najm09 <fnajm09@gmail.com>
2020-11-16 13:45:47 +0530

    solving problems

--- codechefJMI/D.cpp
+++ codechefJMI/D.cpp
@@ -4,28 +4,18 @@ using namespace std;
 int main()
 {
   unordered_map<int, int> m;
-  int n, M, l1, r1;
-  int answere = 0;
-  cin >> n;
-  cin >> l1 >> r1;
-  for (int i = l1; i <= r1; ++i)
-    m[i]++;
-  for (int i = 0; i < n - 1; ++i)
-  {
-    cin >> l1;
-    cin >> r1;
-    for (int i = l1; i <= r1; ++i)
-    {
-      if (m.count(i))
-      {
+  int n, M, l1, r1, answer = 0;
+  cin >> n >> l1 >> r1;
+  for (int i = l1; i <= r1; ++i) m[i]++;
+  for (int i = 0; i < n - 1; ++i){
+    cin >> l1 >> r1;
+    for (int i = l1; i <= r1; ++i){
+      if (m.count(i)){
         m[i]++;
-        if (m[i] == M)
-          answere++;
+        if (m[i] == M) answer++;
       }
     }
   }
-
-  cout << answere << " ";
-
+  cout << answer << " ";
   return 0;
 }
