61f0e7f0ac5e812090fd8abaf7a5456d3e3446d2
najm09 <fnajm09@gmail.com>
2020-12-03 18:00:58 +0530

    increasing order binary search tree

--- leetcode/binarytree/increasingSearch.cpp
+++ leetcode/binarytree/increasingSearch.cpp
@@ -10,13 +10,42 @@
  * };
  */
 class Solution {
+	list<int> List;
 public:
-  TreeNode* increasingBST(TreeNode* root, TreeNode *tail = NULL) {
-  	if(! root) return tail;
-  	TreeNode *tree = increasingBST(root->left, root);
-  	root->left  = NULL;
-  	root->right = increasingBST(root->right, tail);
-  	return tree;
-  }
-  
+	void inorder(TreeNode *root){
+		if(!root ) return ;
+		inorder(root->left);
+		List.push_back(root->val);
+		inorder(root->right);
+	}
+	TreeNode* increasingBST(TreeNode *root){
+		if(!root) return NULL;
+		inorder(root);
+		TreeNode *result = new TreeNode(0);
+		TreeNode *ans = result;
+		while(!List.empty()){
+			result->right = new TreeNode(List.front());
+			List.pop_front();
+			result = result->right;
+		}
+		List.clear();
+		return ans->right;
+	}
+};
+
+class Solution2 {
+	TreeNode *node;
+public:
+	void inorder(TreeNode *root){
+		if(!root ) return ;
+		inorder(root->left);
+		node->right = root->val;
+		node = node->left;
+		inorder(root->right);
+	}
+	TreeNode* increasingBST(TreeNode *root){
+		TreeNode *result = new TreeNode(0);
+		node = result;
+		return result->right;
+	}
 };
