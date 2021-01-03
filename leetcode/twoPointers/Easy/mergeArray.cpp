2d572f0a9ab5e4475bae2f72a9c8895dd575095f
najm09 <fnajm09@gmail.com>
2021-01-03 23:44:15 +0530

    improved to O(1) space

--- leetcode/twoPointers/Easy/mergeArray.cpp
+++ leetcode/twoPointers/Easy/mergeArray.cpp
@@ -6,3 +6,19 @@ public:
 		sort(nums1.begin(), nums1.end());
 	}
 };
+
+class Solution {
+public:
+	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+		int a = m -1;
+		int b = n -1;
+		int idx = m + n - 1;
+		while(a >= 0 && b >= 0){
+			if(nums1[a] > nums2[b]) 
+				nums1[idx--] = nums1[a--];
+			else 
+				nums1[idx--] = nums2[b--];
+		}
+		while(b >= 0) nums1[idx--] = nums2[b--];
+	}
+};
