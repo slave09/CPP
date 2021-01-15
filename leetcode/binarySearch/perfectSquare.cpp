37ce137aac7dada8564ea4d013ae4c4f575b7059
Fakhra Najm <fnajm09@gmail.com>
2021-01-16 00:52:49 +0530

    perfect square

---
+++ leetcode/binarySearch/perfectSquare.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num == 1) return true;
+        long left = 0, right = num / 2;
+        while(left <= right){
+            long mid = left + (right - left) / 2;
+            if(mid * mid == num) return true;
+            else if(mid * mid < num) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
+    }
+};
