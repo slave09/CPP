fedf1c501b03e880ea06400be74d333d506a2bc9
Fakhra Najm <fnajm09@gmail.com>
2021-01-15 16:51:33 +0530

    Goal Parser Interpretation

---
+++ leetcode/String/Easy/cmd.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string res = "";
+        for(int i = 0; i < command.size(); ++i){
+            if(command[i] == 'G') res += 'G';
+            else if(command[i] == '(' && command[i+1] == 'a'){
+                res += "al";
+                i += 3;
+            }
+            else if(command[i] == '(' && command[i + 1] == ')') res += 'o';
+        }
+        return res;
+    }
+};
