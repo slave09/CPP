c2ed929bce51a7c63ec446aa7007b45361f00b86
Fakhra Najm <fnajm09@gmail.com>
2021-02-08 17:43:16 +0530

    binarytree Medium

--- leetcode/Easy/harmonious.cpp
+++ leetcode/Easy/harmonious.cpp
@@ -1,6 +1,8 @@
+
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
+
         if(nums.size() == 1) return 0;
         
         map<int,int> count;
@@ -12,11 +14,14 @@ public:
         int maxSubArrLen = 0, last = INT_MIN, lastCount = -1;
         
         for(auto num : count){
+
             if(last != INT_MIN && num.first - last <= 1)
                 maxSubArrLen = max(maxSubArrLen, num.second + lastCount);
+
             last = num.first;
             lastCount = num.second;
         }
+
         return maxSubArrLen;
     }
 };
