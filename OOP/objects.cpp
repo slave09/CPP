0288af0ea9805813364744922756fe50cead1d99
najm09 <fnajm09@gmail.com>
2021-06-18 22:09:52 +0530

    passing and returning Objects

--- OOP/objects.cpp
+++ OOP/objects.cpp
@@ -14,11 +14,22 @@ public:
 	}
 };
 
+Person modify_details(Person p){
+	p.name = "Nazra Aman";
+	p.email = "amannazra01@gmail.com";
+	p.age = p.age - 1;
+	return p;
+}
+
 int main(){
 	Person first;
 	first.name = "Fakhra Najm";
 	first.age = 20;
 	first.email = "fnajm09@gmail.com";
 	first.read_details();
+	Person modified = modify_details(first);
+	cout << "Name - " << modified.name << endl;
+	cout << "Email - " << modified.email << endl;
+	cout << "Age - " << modified.age << endl;
 	return 0;
 }
