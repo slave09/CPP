8296adbd46871675357770d27c4a4fb7bd312d86
najm09 <fnajm09@gmail.com>
2021-07-16 18:12:08 +0530

    Operator Overloading

---
+++ OOP/operator_overloading.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+class Complex{
+	float real;
+	float imaginary;
+
+public:
+
+	Complex();
+	Complex(float real, float imaginary);
+	Complex(Complex &number);
+
+	float getImaginary() {return imaginary;}
+	float getReal(){return real;}
+
+	void setImaginary(float imaginary){this -> imaginary = imaginary;}
+	void setReal(float real){this -> real = real;}
+
+// Operator overloading 
+	Complex operator+(Complex complex_number);
+
+};
+
+int main(){
+	Complex first(5,5);
+	Complex second(6,9);
+	Complex sum;
+	sum = first + second;
+
+	cout << "First Number : " << first.getReal() << " + " << first.getImaginary() << "i" << endl; 
+	cout << "second Number : " << second.getReal() << " + " << second.getImaginary() << "i" << endl;
+	cout << "Sum : " << sum.getReal() << " + " << sum.getImaginary() << "i" << endl;
+	return 0;
+}
+
+Complex :: Complex(){
+	setReal(0);
+	setImaginary(0);
+}
+
+Complex :: Complex(float real, float imaginary){
+	setReal(real);
+	setImaginary(imaginary);
+}
+
+Complex :: Complex(Complex &number){
+	setReal(number.real);
+	setImaginary(number.imaginary);
+}
+
+Complex Complex :: operator+(Complex complex_number){ 
+
+	Complex sum;
+
+	sum.real = real + complex_number.real;
+	sum.imaginary = imaginary + complex_number.imaginary;
+
+	return sum;
+}
