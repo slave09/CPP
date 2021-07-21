a02a3c6604cfdd50a8b9cd8e3d9e133863b30ccf
najm09 <fnajm09@gmail.com>
2021-07-21 16:23:12 +0530

    code refactor

--- Leetcode/Array/Easy/Missing.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int addUp = 0, total_sum = (nums.size()-1)*nums.size()/2;
-        for(auto num : nums) addUp += num;
-        return (total_sum - addUp);
-    }
-};
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# C++ Programming Language
 
