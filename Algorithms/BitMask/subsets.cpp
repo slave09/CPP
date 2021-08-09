6b1c42142ac4f2bff739b7e219538e5d2caef985
najm09 <fnajm09@gmail.com>
2021-08-09 15:53:08 +0530

    Subsets : Reduced space

--- Algorithms/BitMask/subsets.cpp
+++ Algorithms/BitMask/subsets.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
 	vector<vector<int>> subsets(vector<int>& nums) {
-		vector<vector<int>>subset_array;
 		int size = nums.size();
 		int total_combinations = 1 << size;
 
+		vector<vector<int>>subset_array(total_combinations);
+
 		for(int i = 0; i < total_combinations; ++i){
-			vector<int>curr_combination;
+
 			for(int position = 0; position < size; ++position){
-				if((i >> position) & 1){
-					curr_combination.push_back(nums[position]);
-				}
+				
+				if((i >> position) & 1)
+					subset_array[i].push_back(nums[position]);
 			}
-			subset_array.push_back(curr_combination);
 		}
 		return subset_array;
 	}
