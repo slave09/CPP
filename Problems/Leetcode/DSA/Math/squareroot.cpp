c0763120127232739a2cba5c7958614e14db25c3
najm09 <fnajm09@gmail.com>
2021-08-10 18:48:17 +0530

    Medium problem

--- Problems/Leetcode/DSA/Math/squareroot.cpp
+++
@@ -1,6 +0,0 @@
-class Solution {
-public:
-    int mySqrt(int x) {
-        return pow(x, 0.5);
-    }
-};
--- Problems/codeforces/MAIN/Problem_A/calculate.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-
-int main(){
-	long long range ;
-	std :: cin >> range;
-	std :: cout << (range / 2)*(1 + range / 2) - (range - range/2)*(range - range/2);
-	return 0;
-}
