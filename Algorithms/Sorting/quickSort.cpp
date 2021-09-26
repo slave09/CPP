03c9bf9bb1470aaf3bc005dfdc7edf22819d0027
najm09 <fnajm09@gmail.com>
2021-09-26 14:06:30 +0530

    Quick Sort : code refactor

--- Algorithms/Sorting/quickSort.cpp
+++ Algorithms/Sorting/quickSort.cpp
@@ -1,33 +1,31 @@
 #include<iostream>
 using namespace std;
 
-int partition(int arr[], int low, int high){
-	int pivot = arr[low];
-	int i = low, j = high;
-
-	while(i < j){
-		do{i++;}while(pivot >= arr[i]);
-		do{j--;}while(pivot < arr[j]);
-		if(i < j) swap(arr[j], arr[i]);
-	}
-	swap(arr[j], arr[low]);
-	return j;
+// Taking Last Element as Pivot
+// swapping smaller elements to left
+int partition(int A[], int low, int high){
+  int storeIndex = low;
+  for(int index = low; index <= high; ++index){
+    if(A[index] < A[high] && storeIndex <= high ){
+      swap(A[index], A[storeIndex++]);
+    }
+  }
+  swap(A[storeIndex], A[high]);
+  return storeIndex;
 }
 
-void quickSort(int arr[],int low ,int high){
-	if(low < high){
-		int pivot = partition(arr, low, high);
-		quickSort(arr, low, pivot);
-		quickSort(arr, pivot + 1, high);
-	}
+void quickSort(int A[], int low, int high){
+  if(low > high) return;
+  int pivot = partition(A, low, high);
+  if(pivot >= high) return;
+  quickSort(A, low, pivot);
+  quickSort(A, pivot + 1, high);
 }
 
 int main(){
-	int arr[9] = {6,5,8,9,3,10,15,12,16};
-	quickSort(arr, 0, 8);
-
+	int arr[10] = {5,2,3,1,6,8,4,9,7,10};
+	quickSort(arr, 0, 9);
 	for(auto num : arr) cout << num << " ";
 	cout << endl;
-
 	return 0;
 }
