ca26ca5cdd824b95d00893bea651ec853026a335
najm09 <fnajm09@gmail.com>
2022-02-14 21:21:55 +0530

    Greedy Algorithm : Huffman Coding technique for data compression

--- Algorithms/Greedy/huffmanCoding.cpp
+++ Algorithms/Greedy/huffmanCoding.cpp
@@ -1,82 +1,151 @@
 /*	
 	* Topic : Huffman Coding, a compression method to reduce data size.  
-	* Algorithm : "https://www.youtube.com/watch?v=L5MloiCxHPk&ab_channel=Insidecode"
-	* sort the characters on the basis of their frequency 
+	* Algorithm : 
+		1. "https://www.youtube.com/watch?v=co4_ahEDCho&list=PLDN4rrl48XKpZkf03iYFl-O29szjTrs_O&index=43&t=895s"
+		2. "https://www.youtube.com/watch?v=L5MloiCxHPk&ab_channel=Insidecode"
+	* Take the characters on the basis of their frequency 
 	* 1. Higher frequency character will have lesser number of bits
-	* 2. Lower frequency character will have more number of bits
-	* generate Huffman tree by taking pair of min frequency characters 2 at a time
-	* Perfomr DFS on the tree and generate code for each path by taking left edges as 0s and right Edges as 1s
+	* 2. Lower frequency character will have higher number of bits
+	* generate Huffman tree by taking pair of min frequency characters.
+	* Perfomr DFS on the tree and generate code for each path by taking left edges as 0s and right Edges as 1s.
 	* Tags : Heap, Tree, Map, DFS, BackTracking
 */
 
-#include <bits.stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+#define nullChar  '`'
+#define maxMessageLen 100
+
 class Node{
+public:
 	int frequency;
 	char c;
 	Node *left;
 	Node *right;
-public:
 	Node(char c, int frequency, Node *left, Node *right){
 		this -> c = c;
 		this -> frequency = frequency;
 		this -> left = left;
 		this -> right = right;
 	}
-}
+};
 
 class HuffmanTree{
 	struct comparator{
 		bool operator()(Node *min, Node *max){
-			return min.second > max.second;
+			return min -> frequency > max -> frequency;
 		}
 	};
 	unordered_map<char, int>frequencyLookup;
-	priority_queue<Node*, vector<Node*>, comparator>pq;
+	priority_queue<Node*, vector<Node*>, comparator>heap;
 	unordered_map<char, string>codeLookup; // Store compressed message code
+	Node *root = NULL;
+
 public:
 
 	HuffmanTree(string message){
 		for(auto m : message) frequencyLookup[m]++;
-		for(auto freq : frequencyLookup) pq.push(new Node(freq.first, freq.second, NULL, NULL));
-		Node *tree = buildTree();
-		// GenerateCodeMap
-		// Encode
-		// Decode
+		for(auto freq : frequencyLookup) 
+			heap.push(new Node(freq.first, freq.second, NULL, NULL));
+		root = buildTree();
+		GenerateCodeMap(root, "");
+		// string encodedMessage = Encode(message);
+		// string decodedMessage = Decode(root, encodedMessage);
 	}
 
+	Node* getRoot(){return this -> root;}
+
+	/*
+		* Generates binary tree
+		* @return pointer to the root of binary tree
+	*/
 	Node * buildTree(){
-		while(pq.size() > 1){
-			Node *left = pq.top();
-			pq.pop();
-			Node *right = pq.top();
-			pq.pop();
-			Node *parent = new Node(nullptr, left -> frequency + right -> frequency, left, right);
-			pq.push(parent);
+		while(heap.size() > 1){
+			Node *left = heap.top();
+			heap.pop();
+			Node *right = heap.top();
+			heap.pop();
+			Node *parent = new Node(nullChar, left -> frequency + right -> frequency, left, right);
+			heap.push(parent);
 		}
-		return pq.top();
+		return heap.top();
 	}
 
 	/*
-		* Implement GenerateCodeMap
-		* @param tree Huffman tree
+		* Generates a mapping table of characters and their codes
+		* @param root pointer to the root of binary tree
+		* @param code compressed message code
 	*/
 
-	/*
-		* Implement Encode
-		* @param message original string
-		* @Return compressed message
-	*/
+	void GenerateCodeMap(Node *root, string code){
+		if(root -> c != nullChar) codeLookup[root -> c] = code;
+		else{
+			code.push_back('0');
+			GenerateCodeMap(root -> left, code);
+			code.pop_back();
+			code.push_back('1');
+			GenerateCodeMap(root -> right, code);
+			code.pop_back();
+		}
+	}
 
 	/*
-		* Implement Decode
-		* @Return original message
-		* @param message encoded string
+		* Encode messages
+		* @param message original message string 
+		* @return compressed message
 	*/
+
+	string Encode(string message){
+		string encodedMessage = "";
+		for(auto m : message){
+			encodedMessage += codeLookup[m];
+		}
+		return encodedMessage;
+	}
+
+	/*
+		* Decode messages
+		* @param message to be decoded
+		* @return string of original message
+	*/
+
+	string getRootMessage(char c, int freq){
+		string decodedMessage = "";
+		while(freq--){
+			decodedMessage.push_back(c);
+		}
+		return decodedMessage;
+	}
+
+	string Decode(Node *root, string encodedMessage){
+		if(root -> c != nullChar) 
+			return getRootMessage(root -> c, root -> frequency);
+
+		string decodedMessage = "";
+		Node *node = root;
+
+		for(auto bit : encodedMessage){
+			if(bit == '0') node = node -> left;
+			else node = node -> right;
+			if(node -> c != nullChar){
+			 decodedMessage.push_back(node -> c);
+			 node = root;
+			}
+		}
+		return decodedMessage;
+	}
 };
 
 
 int main(){
+	string message;
+	cout << "Enter message : ";
+	getline(cin, message);
+	HuffmanTree compress(message);
+	string encodedMessage = compress.Encode(message);
+	cout << encodedMessage<< endl;
+	string decodedMessage = compress.Decode(compress.getRoot(), encodedMessage);
+	cout << decodedMessage << endl;
 	return 0;
 }
